d596c68a4b36acae0e2f8d7413a0e07fa481daef
Oleg Kokarev <dokorev@gmail.com>
2021-08-14 18:12:45 -0400

    https://acmp.ru/index.asp?main=task&id_task=1

---
+++ ok.practice/src/AplusB.java
@@ -0,0 +1,15 @@
+// A+B problem for acmp.ru
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int ans = a + b;
+        out.println(ans);
+        out.close();
+    }
+}

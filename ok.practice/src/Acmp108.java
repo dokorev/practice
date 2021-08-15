23ec470f117c54b464eea4e5c4c43f172bd29de6
Oleg Kokarev <dokorev@gmail.com>
2021-08-15 11:27:54 -0400

    https://acmp.ru/index.asp?main=task&id_task=108

---
+++ ok.practice/src/Acmp108.java
@@ -0,0 +1,16 @@
+// https://acmp.ru/index.asp?main=task&id_task=108
+import java.io.*;
+import java.util.*;
+
+public class Acmp108 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        // your solution
+        // vvvvvvvvvv
+        int n = in.nextInt();
+        out.println(n);
+        // ^^^^^^^^^^
+        out.close();
+    }
+}

7d39beb30d1910e2bfd0c2245e1abe2ac2fe5edc
Oleg Kokarev <dokorev@gmail.com>
2021-08-15 11:37:24 -0400

    https://acmp.ru/index.asp?main=task&id_task=33

---
+++ ok.practice/src/Acmp33.java
@@ -0,0 +1,18 @@
+// https://acmp.ru/index.asp?main=task&id_task=33
+import java.io.*;
+import java.util.*;
+
+public class Acmp33 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        // your solution
+        // vvvvvvvvvv
+        int g = in.nextInt();
+        int l = in.nextInt();
+        out.println(l-1);
+        out.println(g-1);
+        // ^^^^^^^^^^
+        out.close();
+    }
+}

ee6bb7ddec0189a2e30376bd354e4d0610dc64ed
Oleg Kokarev <dokorev@gmail.com>
2021-08-15 11:38:22 -0400

    acmp Solution Template

--- ok.practice/src/Main.java
+++ ok.practice/src/Main.java
@@ -1,4 +1,4 @@
-// A+B problem for acmp.ru
+// acmp.ru
 import java.io.*;
 import java.util.*;
 
@@ -6,10 +6,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-        int a = in.nextInt();
-        int b = in.nextInt();
-        int ans = a + b;
-        out.println(ans);
+        // your solution
+        // vvvvvvvvvv
+        // ^^^^^^^^^^
         out.close();
     }
 }

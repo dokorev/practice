b3eea9af2e7c113e7ac16630f9a2639701d87efe
Oleg Kokarev <dokorev@gmail.com>
2021-08-17 17:56:22 -0400

    https://acmp.ru/index.asp?main=task&id_task=1038

---
+++ ok.practice/src/Acmp1038.java
@@ -0,0 +1,17 @@
+// acmp.ru
+import java.io.*;
+import java.util.*;
+
+public class Acmp1038 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        // your solution
+        // vvvvvvvvvv
+        int n = in.nextInt();
+        int ans = (n+9)/10;
+        out.println(ans);
+        // ^^^^^^^^^^
+        out.close();
+    }
+}

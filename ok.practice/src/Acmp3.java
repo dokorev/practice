5836932bc24746cefd3cd2ce967e1f816a2c8c60
Oleg Kokarev <dokorev@gmail.com>
2021-08-18 17:48:11 -0400

    https://acmp.ru/index.asp?main=task&id_task=3

---
+++ ok.practice/src/Acmp3.java
@@ -0,0 +1,17 @@
+// acmp.ru
+import java.io.*;
+import java.util.*;
+
+public class Acmp3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        // your solution
+        // vvvvvvvvvv
+        long n = in.nextInt();
+        long ans = n*n;
+        out.println(ans);
+        // ^^^^^^^^^^
+        out.close();
+    }
+}

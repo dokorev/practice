cfe5d3dce4467854c0be5d8bce61aeba54cb432c
Oleg Kokarev <dokorev@gmail.com>
2021-08-19 14:03:05 -0400

    https://acmp.ru/index.asp?main=task&id_task=528

---
+++ ok.practice/src/Acmp528.java
@@ -0,0 +1,18 @@
+// acmp.ru
+import java.io.*;
+import java.util.*;
+
+public class Acmp528 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        // your solution
+        // vvvvvvvvvv
+        long n = in.nextLong();
+        long k = in.nextLong();
+        long ans = k+1 + ((n-2) * (1 + k) * k)/2;
+        out.println(ans);
+        // ^^^^^^^^^^
+        out.close();
+    }
+}

a1fc78bb194a81225bcea93a0d83214d7bea2689
Oleg Kokarev <dokorev@gmail.com>
2021-08-23 15:39:19 -0400

    https://acmp.ru/index.asp?main=task&id_task=504

--- ok.practice/src/Acmp504.java
+++ ok.practice/src/Acmp504.java
@@ -2,7 +2,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Acmp52 {
+public class Acmp504 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);

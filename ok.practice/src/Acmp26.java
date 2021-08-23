62942804fb6024d28d182f1bbefd962c702d946a
Oleg Kokarev <dokorev@gmail.com>
2021-08-23 15:56:32 -0400

    https://acmp.ru/index.asp?main=task&id_task=26

--- ok.practice/src/Acmp26.java
+++ ok.practice/src/Acmp26.java
@@ -2,20 +2,20 @@
 import java.io.*;
 import java.util.*;
 
-public class Acmp52 {
+public class Acmp26 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
         // your solution
         // vvvvvvvvvv
-        int n = in.nextInt();
-        int d1 = n/100000;
-        int d2 = n/10000 % 10;
-        int d3 = n/1000 % 10;
-        int d4 = n/100 % 10;
-        int d5 = n/10 % 10;
-        int d6 = n % 10;
-        if (d1 + d2 + d3 == d4 + d5 + d6) {
+        int x1 = in.nextInt();
+        int y1 = in.nextInt();
+        int r1 = in.nextInt();
+        int x2 = in.nextInt();
+        int y2 = in.nextInt();
+        int r2 = in.nextInt();
+        int d2 = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+        if ((r1 - r2) * (r1 - r2) <= d2 && d2 <= (r1 + r2) * (r1 + r2)) {
             out.println("YES");
         } else {
             out.println("NO");

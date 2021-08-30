7c5ecf844e13b6bf221982d93ba1083b900434bb
Oleg Kokarev <dokorev@gmail.com>
2021-08-30 14:43:14 -0400

    https://acmp.ru/index.asp?main=task&id_task=1120

---
+++ ok.practice/src/Acmp1120.java
@@ -0,0 +1,30 @@
+// acmp.ru
+import java.io.*;
+import java.util.*;
+
+public class Acmp1120 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        // your solution
+        // vvvvvvvvvv
+        int x1 = in.nextInt();
+        int y1 = in.nextInt();
+        int x2 = in.nextInt();
+        int y2 = in.nextInt();
+        if (x2 - x1 == y2 - y1) {
+            out.println("YES");
+        } else if (x2 - x1 == y1 - y2) {
+            out.println("YES");
+        } else if (x1 - x2 == y2 - y1) {
+            out.println("YES");
+        } else if (x1 - x2 == y1 - y2) {
+            out.println("YES");
+        } else {
+            out.println("NO");
+        }
+
+        // ^^^^^^^^^^
+        out.close();
+    }
+}

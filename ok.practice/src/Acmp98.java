9000dc68f269e4690506c3199f22d60e0ba534aa
Oleg Kokarev <dokorev@gmail.com>
2021-10-17 19:38:54 -0400

    https://acmp.ru/index.asp?main=task&id_task=98

---
+++ ok.practice/src/Acmp98.java
@@ -0,0 +1,44 @@
+// acmp.ru
+import java.io.*;
+import java.util.*;
+public class Main {
+
+    // Functions if Needed
+    // vvvvvvvvv
+
+    // ^^^^^^^^^
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        try {
+            // your solution
+            // vvvvvvvvvv
+            int n = in.nextInt();
+            int a[] = new int [n];
+            for (int i = 0; i < n; i++) {
+                a[i] = in.nextInt();
+            }
+            int first = 0;
+            int last = n - 1;
+            int counter[] = new int [2];
+            int player = 0;
+            while (first <= last) {
+                if (a[last] > a[first]) {
+                    // last >
+                    counter[player] += a[last];
+                    last--;
+                } else {
+                    // first <=
+                    counter[player] += a[first];
+                    first++;
+                }
+                player ^= 1;
+            }
+            out.println(counter[0] + ":" + counter[1]);
+            // ^^^^^^^^^^
+        } finally {
+            out.close();
+        }
+    }
+}

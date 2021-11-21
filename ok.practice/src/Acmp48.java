1da8af19d9ac63fdd8609d4688b14f115352ade2
Oleg Kokarev <dokorev@gmail.com>
2021-11-20 22:20:49 -0500

    https://acmp.ru/index.asp?main=task&id_task=48

---
+++ ok.practice/src/Acmp48.java
@@ -0,0 +1,29 @@
+// acmp.ru
+import java.io.*;
+import java.util.*;
+public class Acmp48 {
+    // functions if needed
+    // vvvvvvvvv
+
+    // ^^^^^^^^^
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        try {
+            // your solution
+            // vvvvvvvvvv
+            String n = in.next();
+            out.print('1');
+            for (int i = n.length() - 1; i >= 0; i--) {
+                if (n.charAt(i) == '0') {
+                    out.print('0');
+                } else  {
+                    break;
+                }
+            }
+            // ^^^^^^^^^^
+        } finally {
+            out.close();
+        }
+    }
+}
--- ok.practice/src/Main.java
+++ ok.practice/src/Main.java
@@ -12,27 +12,15 @@
             try {
                 // your solution
                 // vvvvvvvvvv
-                int sizeI = in.nextInt();
-                int sizeJ = in.nextInt();
-                char a[][] = new char[1 + sizeI + 1][1 + sizeJ + 1];
-                for (int i = 0; i < a.length; i++) {
-                    Arrays.fill(a[i], '.');
-                }
-                for (int i = 1; i <= sizeI; i++) {
-                    String s = in.next();
-                    for (int j = 1; j <= sizeJ; j++) {
-                        a[i][j] = s.charAt(j - 1);
+                String n = in.next();
+                out.print('1');
+                for (int i = n.length() - 1; i >= 0; i--) {
+                    if (n.charAt(i) == '0') {
+                        out.print('0');
+                    } else  {
+                        break;
                     }
                 }
-                int count = 0;
-                for (int i = 1; i <= sizeI; i++) {
-                    for (int j = 1; j <= sizeJ; j++) {
-                        if (a[i][j] == '.' && a[i+1][j] == '.' && a[i-1][j] == '.' && a[i][j+1] == '.' && a[i][j-1] == '.' ) {
-                            count++;
-                        }
-                    }
-                }
-                out.println(count);
                 // ^^^^^^^^^^
             } finally {
                 out.close();

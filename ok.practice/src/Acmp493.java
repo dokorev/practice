2db9d88f397ad43477d4a19df2289542ad83d4bd
Oleg Kokarev <dokorev@gmail.com>
2021-11-20 22:00:39 -0500

    https://acmp.ru/index.asp?main=task&id_task=493

---
+++ ok.practice/src/Acmp493.java
@@ -0,0 +1,41 @@
+// acmp.ru
+import java.io.*;
+import java.util.*;
+public class Acmp493 {
+    // functions if needed
+    // vvvvvvvvv
+
+    // ^^^^^^^^^
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        try {
+            // your solution
+            // vvvvvvvvvv
+            int sizeI = in.nextInt();
+            int sizeJ = in.nextInt();
+            char a[][] = new char[1 + sizeI + 1][1 + sizeJ + 1];
+            for (int i = 0; i < a.length; i++) {
+                Arrays.fill(a[i], '.');
+            }
+            for (int i = 1; i <= sizeI; i++) {
+                String s = in.next();
+                for (int j = 1; j <= sizeJ; j++) {
+                    a[i][j] = s.charAt(j - 1);
+                }
+            }
+            int count = 0;
+            for (int i = 1; i <= sizeI; i++) {
+                for (int j = 1; j <= sizeJ; j++) {
+                    if (a[i][j] == '.' && a[i+1][j] == '.' && a[i-1][j] == '.' && a[i][j+1] == '.' && a[i][j-1] == '.' ) {
+                        count++;
+                    }
+                }
+            }
+            out.println(count);
+            // ^^^^^^^^^^
+        } finally {
+            out.close();
+        }
+    }
+}
--- ok.practice/src/Main.java
+++ ok.practice/src/Main.java
@@ -12,21 +12,27 @@
             try {
                 // your solution
                 // vvvvvvvvvv
-                int cur = 0;
-                int min = 0;
-                int max = 0;
-                for (char c : in.next().toCharArray()) {
-                    if (c == '1') {
-                        cur++;
-                    } else if (c == '2') {
-                        cur--;
-                    } else {
-                        throw new Error();
-                    }
-                    min = Math.min(min, cur);
-                    max = Math.max(max, cur);
+                int sizeI = in.nextInt();
+                int sizeJ = in.nextInt();
+                char a[][] = new char[1 + sizeI + 1][1 + sizeJ + 1];
+                for (int i = 0; i < a.length; i++) {
+                    Arrays.fill(a[i], '.');
                 }
-                out.println(max - min + 1);
+                for (int i = 1; i <= sizeI; i++) {
+                    String s = in.next();
+                    for (int j = 1; j <= sizeJ; j++) {
+                        a[i][j] = s.charAt(j - 1);
+                    }
+                }
+                int count = 0;
+                for (int i = 1; i <= sizeI; i++) {
+                    for (int j = 1; j <= sizeJ; j++) {
+                        if (a[i][j] == '.' && a[i+1][j] == '.' && a[i-1][j] == '.' && a[i][j+1] == '.' && a[i][j-1] == '.' ) {
+                            count++;
+                        }
+                    }
+                }
+                out.println(count);
                 // ^^^^^^^^^^
             } finally {
                 out.close();

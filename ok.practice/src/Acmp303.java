67a83bc7411fea73fadaa1115afd811a7e36259f
Oleg Kokarev <dokorev@gmail.com>
2021-12-11 18:41:15 -0500

    https://acmp.ru/index.asp?main=task&id_task=303

---
+++ ok.practice/src/Acmp303.java
@@ -0,0 +1,37 @@
+// acmp.ru
+import java.io.*;
+import java.util.*;
+public class Acmp303 {
+    // functions if needed
+    // vvvvvvvvv
+    public static int plusMinSum(String s) {
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int d = s.charAt(i) - '0';
+            if (i % 2 == 0) {
+                sum += d;
+            } else {
+                sum -= d;
+            }
+        }
+        return sum;
+    }
+    // ^^^^^^^^^
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        try {
+            // your solution
+            // vvvvvvvvvv
+            String s= in.next();
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < s.length(); i++) {
+                max =  Math.max(max, plusMinSum(s.substring(0, i) + s.substring(i + 1)));
+            }
+            out.println(max);
+            // ^^^^^^^^^^
+        } finally {
+            out.close();
+        }
+    }
+}
--- ok.practice/src/Main.java
+++ ok.practice/src/Main.java
@@ -4,7 +4,18 @@
     public class Main {
         // functions if needed
         // vvvvvvvvv
-
+        public static int plusMinSum(String s) {
+            int sum = 0;
+            for (int i = 0; i < s.length(); i++) {
+                int d = s.charAt(i) - '0';
+                if (i % 2 == 0) {
+                    sum += d;
+                } else {
+                    sum -= d;
+                }
+            }
+            return sum;
+        }
         // ^^^^^^^^^
         public static void main(String[] args) {
             Scanner in = new Scanner(System.in);
@@ -12,15 +23,12 @@
             try {
                 // your solution
                 // vvvvvvvvvv
-                String s = in.nextLine();
-                int n = in.nextInt();
-                for (int radix = 2; radix <= 36; radix++) {
-                    if (Integer.toString(n, radix).toUpperCase().compareTo(s) == 0) {
-                        out.println(radix);
-                        return;
-                    }
+                String s= in.next();
+                int max = Integer.MIN_VALUE;
+                for (int i = 0; i < s.length(); i++) {
+                    max =  Math.max(max, plusMinSum(s.substring(0, i) + s.substring(i + 1)));
                 }
-                out.println(0);
+                out.println(max);
                 // ^^^^^^^^^^
             } finally {
                 out.close();

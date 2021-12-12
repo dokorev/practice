708071b5f199e2b0f6e0d09fccf7f0841b99a596
Oleg Kokarev <dokorev@gmail.com>
2021-12-11 20:46:17 -0500

    https://acmp.ru/index.asp?main=task&id_task=278

---
+++ ok.practice/src/Acmp278.java
@@ -0,0 +1,33 @@
+// acmp.ru
+import java.io.*;
+import java.util.*;
+public class Acmp278 {
+    // functions if needed
+    // vvvvvvvvv
+
+    // ^^^^^^^^^
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        try {
+            // your solution
+            // vvvvvvvvvv
+            String s= in.next();
+            String t=  in.next();
+            int si = 0;
+            for (int ti = 0; ti < t.length(); ti++) {
+                if (s.charAt(si) == t.charAt(ti)) {
+                    si++;
+                    if (si == s.length()) {
+                        out.println("YES");
+                        return;
+                    }
+                }
+            }
+            out.println("NO");
+            // ^^^^^^^^^^
+        } finally {
+            out.close();
+        }
+    }
+}
--- ok.practice/src/Main.java
+++ ok.practice/src/Main.java
@@ -4,23 +4,7 @@
     public class Main {
         // functions if needed
         // vvvvvvvvv
-        public static boolean sameChars(String s) {
-            for (int i  = 1;i < s.length(); i++) {
-                if (s.charAt(i) != s.charAt(0)) {
-                   return false;
-                }
-            }
-            return true;
-        }
 
-        public static boolean isPal(String s) {
-            for (int i = 0; i < s.length() - 1 - i; i++) {
-                if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
-                    return false;
-                }
-            }
-            return true;
-        }
         // ^^^^^^^^^
         public static void main(String[] args) {
             Scanner in = new Scanner(System.in);
@@ -29,14 +13,18 @@
                 // your solution
                 // vvvvvvvvvv
                 String s= in.next();
-                if (sameChars(s)) {
-                    out.println("NO SOLUTION");
-                    return;
-                } else if (isPal(s)) {
-                    out.println(s.substring(1));
-                } else {
-                    out.println(s);
+                String t=  in.next();
+                int si = 0;
+                for (int ti = 0; ti < t.length(); ti++) {
+                    if (s.charAt(si) == t.charAt(ti)) {
+                        si++;
+                        if (si == s.length()) {
+                            out.println("YES");
+                            return;
+                        }
+                    }
                 }
+                out.println("NO");
                 // ^^^^^^^^^^
             } finally {
                 out.close();

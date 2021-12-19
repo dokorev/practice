0b1966c629d09f212239153e6718d67bae95b579
Oleg Kokarev <dokorev@gmail.com>
2021-12-19 13:14:41 -0500

    https://acmp.ru/index.asp?main=task&id_task=73

--- ok.practice/src/Acmp73b.java
+++ ok.practice/src/Acmp73b.java
@@ -3,7 +3,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Acmp73b {
     // functions if needed
     // vvvvvvvvv
     final static char code[] = "0123456789ABCDEFGHIJKLMNOPQ".toCharArray();
@@ -22,21 +22,16 @@ public class Main {
                 int c = s.charAt(pos);
                 int cd1 = 1;
                 for (int j = 0; j < code.length; j++) {
-                    char n = code[j];
-                    if (c == n) {
+                    if (c == code[j]) {
                         cd1 = j;
                         break;
                     }
                 }
                 // need to cd1 - i to get ans (in decode)
-                for (int i = 0; i <= pos + 1; i++) {
-                    cd1--;
-                    if (cd1 < 0) {
-                        cd1 = 26;
-                    }
-                }
-                char m = decode[cd1];
-                ans = ans + m;
+                // (a - b) % m = (a + (m - b % m)) % m
+                // (3 - 5) % 4 = (3 + (4 - (5 % 4))) % 4
+                cd1 = (cd1 + 27 - (pos + 2) % 27) % 27;
+                ans += decode[cd1];
             }
             out.println(ans);
             // ^^^^^^^^^^

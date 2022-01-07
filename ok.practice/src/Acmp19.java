18da09e0019dd686d9806f3f25df44e7eef5fdd9
Oleg Kokarev <dokorev@gmail.com>
2022-01-07 18:10:02 -0500

    https://acmp.ru/index.asp?main=task&id_task=19

---
+++ ok.practice/src/Acmp19.java
@@ -0,0 +1,52 @@
+// acmp.ru
+//
+// ACMP 570 with WA 11
+// possible problem is
+// 4 4
+// * . . .
+// . * . .
+// . . * .
+// . . . *
+// SQUARE <-- should not be square as the original square cannot be 2x2
+
+import java.io.*;
+import java.util.*;
+
+public class Acmp19 {
+    // functions if needed
+    // vvvvvvvvv
+
+
+    // ^^^^^^^^^
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        try {
+            // your solution
+            // vvvvvvvvvv
+            String q = in.next();
+            char q1 = q.charAt(0);
+            char qd = q.charAt(1);
+            String r = in.next();
+            char r1 = r.charAt(0);
+            char rd = r.charAt(1);
+            String k = in.next();
+            char k1 = k.charAt(0);
+            char kd = k.charAt(1);
+            int count = 0;
+            for (char let = 'A'; let <= 'H'; let++) {
+                for (char dig = '1'; dig <= '8'; dig++) {
+                    if (!(let == q1 && dig == qd || let == r1 && dig == rd || let == k1 && dig == kd)
+                            && ((let - k1) * (let - k1) + (dig - kd) * (dig - kd) == 5 || (let == r1 || dig == rd)
+                            || (let == q1 || dig == qd || Math.abs(let - q1) == Math.abs(dig - qd)))) {
+                        count++;
+                    }
+                }
+            }
+            out.println(count);
+            // ^^^^^^^^^^
+        } finally {
+            out.close();
+        }
+    }
+}
--- ok.practice/src/Main.java
+++ ok.practice/src/Main.java
@@ -3,10 +3,10 @@
 // ACMP 570 with WA 11
 // possible problem is
 // 4 4
-// *...
-// .*..
-// ..*.
-// ...*
+// * . . .
+// . * . .
+// . . * .
+// . . . *
 // SQUARE <-- should not be square as the original square cannot be 2x2
 
 import java.io.*;
@@ -16,38 +16,6 @@ public class Main {
     // functions if needed
     // vvvvvvvvv
 
-    public static int counter(char mm[][], int h, int w, int lb, int rb, int top, int bottom) {
-        int count = 0;
-        for (int r = top; r <= bottom; r++) {
-            for (int c = lb; c <= rb; c++) {
-                if (mm[r][c] == '*') {
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
-
-    public static boolean check(char mm[][], int h, int w, int lb, int rb, int top, int bottom) {
-        if (rb - lb != bottom - top) {
-            return false;
-        }
-        if (lb < 0 || top < 0) {
-            return false;
-        }
-        if (rb >= w) {
-            return false;
-        }
-        if (bottom >= h) {
-            return false;
-        }
-        if (rb < lb) {
-            return false;
-        }
-        int expected = (rb - lb + 1) * (rb - lb + 1);
-        int actual = counter(mm, h, w, lb, rb, top, bottom);
-        return expected == actual;
-    }
 
     // ^^^^^^^^^
     public static void main(String[] args) {
@@ -56,45 +24,26 @@ public class Main {
         try {
             // your solution
             // vvvvvvvvvv
-            int h = in.nextInt();
-            int w = in.nextInt();
-            in.nextLine();
-            char mm[][] = new char[h][w];
-            for (int i = 0; i < h; i++) {
-                String r = in.nextLine();
-                mm[i] = r.toCharArray();
-            }
-            // find boundaries
-            int lb = w + 1;
-            int rb = 0;
-            int top = h + 1;
-            int bot = 0;
-            for (int r = 0; r < h; r++) {
-                for (int c = 0; c < w; c++) {
-                    if (mm[r][c] == '*') {
-                        rb = Math.max(rb, c);
-                        lb = Math.min(lb, c);
-                        top = Math.min(top, r);
-                        bot = Math.max(bot, r);
-                    }
-                }
-            }
-            // check all squares lb,lb+1,lb+2 * rb-2,rb-1,rb * top-2,top-1,top * bot,bot+1,bot+1
-            boolean ans = false;
-            for (int l = lb; l <= lb+2; l++) {
-                for (int r = rb-2; r <= rb; r++) {
-                    for (int t = top; t <= top+2; t++) {
-                        for (int b = bot-2; b <= bot; b++) {
-                            ans |= check(mm, h, w, l, r, t, b);
-                        }
-                    }
-                }
-            }
-            if (ans) {
-                out.println("SQUARE");
-            } else {
-                out.println("CIRCLE");
-            }
+           String q = in.next();
+           char q1 = q.charAt(0);
+           char qd = q.charAt(1);
+           String r = in.next();
+           char r1 = r.charAt(0);
+           char rd = r.charAt(1);
+           String k = in.next();
+           char k1 = k.charAt(0);
+           char kd = k.charAt(1);
+           int count = 0;
+           for (char let = 'A'; let <= 'H'; let++) {
+               for (char dig = '1'; dig <= '8'; dig++) {
+                   if (!(let == q1 && dig == qd || let == r1 && dig == rd || let == k1 && dig == kd)
+                           && ((let - k1) * (let - k1) + (dig - kd) * (dig - kd) == 5 || (let == r1 || dig == rd)
+                           || (let == q1 || dig == qd || Math.abs(let - q1) == Math.abs(dig - qd)))) {
+                       count++;
+                   }
+               }
+           }
+           out.println(count);
             // ^^^^^^^^^^
         } finally {
             out.close();
